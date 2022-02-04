17ea4cb9240f1ea5f4d8d3d1e17f0a4f49879a67
Raymond Chen <ahrchen@gmail.com>
2022-02-03 16:25:03 -0800

    Initial Commit

---
+++ TempConversion/TempConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConversionApp.swift
+//  TempConversion
+//
+//  Created by Raymond Chen on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TempConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
